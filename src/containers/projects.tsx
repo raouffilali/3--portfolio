2dee77b47692e4d62b8c8a3c71e37e19462d406d
Abderraouf FILALI <filaliabderaouf@yahoo.com>
2024-03-10 17:22:10 +0100

    Refactor projects section to use async/await and add type annotation

--- src/containers/projects.tsx
+++ src/containers/projects.tsx
@@ -2,17 +2,18 @@ import ProjectItem from "@/components/projects/project-item";
 import SectionTitle from "@/components/section-title";
 import { Effect } from "@/components/ui/effects";
 import { getProjects } from "@/lib/projects";
+import { Project } from "@/types/interfaces";
 import React from "react";
 
 const ProjectsSection = async () => {
-  const projects = await getProjects();
+  const { data: projects } = await getProjects();
 
   return (
     <section id="projects" className="relative space-y-6">
       <Effect className="top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]" />
       <SectionTitle title="projects" description="some things i've built" />
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {projects.map((project) => (
+        {projects.map((project: Project) => (
           <ProjectItem key={project._id} project={project} />
         ))}
       </div>
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -4,19 +4,22 @@ import { groq } from "next-sanity";
 
 export async function getProjects() {
   const query = groq`
-        *[_type=="projects"]{
-            _id,
-            title,
-            description,
-            images,
-            tags,
-            demoLink,
-            githubLink,
-            publishedAt,
-            isResponsive
-      }
-      `;
+    *[_type=="projects"]{
+      _id,
+      title,
+      description,
+      images,
+      tags,
+      demoLink,
+      githubLink,
+      publishedAt,
+      isResponsive
+    }
+  `;
 
   const data = await client.fetch(query);
-  return data as Project[];
+  return {
+    data: data as Project[],
+    revalidate: 60 // Set the revalidation time in seconds
+  };
 }
